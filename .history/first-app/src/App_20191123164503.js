21a778691272e7fdce301389ff3cdfeab00dbd95
Ismail <salmi.ismail8u@gmail.com>
2019-11-23 17:25:51 +0100

    event + changes

---
+++ .history/first-app/src/App_20191123164503.js
@@ -0,0 +1,71 @@
+import React , {Component , Fragment} from 'react';
+import './App.css';
+import Membre from './components/Membre'
+import Button from './components/Button';
+import Input from './components/Input';
+
+const items = {
+    membre1:{
+      name:'ismail',
+      age: 25
+    },
+    membre2:{
+      name:'houda',
+      age: 22
+    }
+
+}
+isShow: false
+
+
+class App extends Component{
+state = { items , isShow }
+
+handleClick = num => {
+  const items = this.state.items;
+  items.membre1.age +=num
+  this.setState({items})
+}
+
+hadnleChange = event => {
+  const items = this.state.items
+  const name = event.target.value
+  items.membre1.name =name
+  this.setState({items})
+}
+
+hadnleShowDescription = () =>{
+  const isShow = this.state.isShow
+  this.setState({isShow})
+}
+
+
+render () {
+  const { items } = this.state;
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>App Work</h1>
+        <Input  name={items.membre1.name} changeValue ={(event) => this.hadnleChange(event)} />
+        <Membre name={items.membre1.name}  age={items.membre1.age}/>
+        <Membre name={items.membre2.name} age={items.membre2.age}>
+          {
+            isShow ? <strong>je viens d un children</strong> : null
+          }
+          <button onClick={this.hadnleShowDescription}>
+            {
+              isShow ? 'Cacher' : 'Montrer'
+            }
+          </button>
+        
+        </Membre>
+        <Button up ={() => this.handleClick(2)} />
+      </div>
+      
+
+    </Fragment>
+        );
+   }
+}
+
+export default App;
---
+++ .history/first-app/src/components/Button_20191123164041.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({up}) => {
+
+    return (
+        <button onClick ={up}>Up</button>
+    )
+}
+
+
+
+
+export default Button
